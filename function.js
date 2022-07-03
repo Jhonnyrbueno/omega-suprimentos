3c56c8685e2efe46c01f675b05631e91da5604a2
Jhonny Bueno <jhonnyrbueno@gmail.com>
2022-07-03 01:09:00 -0300

    Commits

--- function.js
+++ function.js
@@ -15,5 +15,21 @@ animeScroll ()
 
 window.addEventListener("scroll", ()=>{
     animeScroll();
-})
+});
 
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+
+idx++;
+
+    if(idx > img.length -1){
+    idx = 0;
+    }
+    imgs.style.transform = `translateX(${-idex * 500})`;
+}
+
+setInterval(carrossel, 1800);
