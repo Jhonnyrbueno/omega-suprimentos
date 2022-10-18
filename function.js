ee2195d8a84e0782a033e679d4fbb6b6edbf42b5
Jhonny Bueno <jhonnyrbueno@gmail.com>
2022-10-17 23:13:54 -0300

    commit

--- function.js
+++ function.js
@@ -16,3 +16,22 @@ animeScroll ()
 window.addEventListener("scroll", ()=>{
     animeScroll();
 });
+
+'use strict';
+
+var onTop = false;
+
+// JQuery
+$(window).scroll(function() {
+  let scrollPosition = $(window).scrollTop();
+  
+  if(scrollPosition < 10 && !onTop) {
+    onTop = true;
+    $('.navbar').addClass('onTop');
+  }
+  
+  if(scrollPosition > 10 && onTop) {
+    onTop = false;
+    $('.navbar').removeClass('onTop');
+  }
+});
