9a705614a511a2e90693457efce9e9a88eb6d6b5
Jhonny Bueno <jhonnyrbueno@gmail.com>
2022-10-23 04:37:21 -0300

    commits

--- function.js
+++ function.js
@@ -35,3 +35,17 @@ $(window).scroll(function() {
     $('.navbar').removeClass('onTop');
   }
 });
+
+
+$(document).ready(function () {
+  $("body").append("<a href='#' class='back-to-top'><i class='fa fa-arrow-circle-up' aria-hidden='true'></i></a>"),
+      $(function toTop() {
+          $(window).scroll(function () {
+              $(this).scrollTop() > 400 ? $(".back-to-top").fadeIn() : $(".back-to-top").fadeOut()
+          }), $(".back-to-top").click(function () {
+              return $("html, body").animate({
+                  scrollTop: 0
+              }, 900), !1
+          })
+      });
+})
